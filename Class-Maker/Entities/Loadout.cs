298dec7512ae1b8be12e54deafcc9e538d01ef1b
Aquerr <nerdianin@gmail.com>
2016-09-22 22:42:44 +0200

    Simple loadout class.

---
+++ Class-Maker/Entities/Loadout.cs
@@ -0,0 +1,9 @@
+﻿namespace Class_Maker.Entities
+{
+    public class Loadout
+    {
+        public int LoadoutId { get; set; }
+        public string Name { get; set; }
+        //TODO: More property have to be added here.
+    }
+}
