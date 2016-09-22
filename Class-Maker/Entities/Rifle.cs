07d3d3eab94aaf678d1f8be05ce75cd39f5aee00
Aquerr <nerdianin@gmail.com>
2016-09-22 23:06:07 +0200

    Added Rifle class

---
+++ Class-Maker/Entities/Rifle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Maker.Entities
+{
+    public class Rifle
+    {
+        public int WeaponId { get; set; }
+        public string Name { get; set; }
+       // public Attachment Attachment { get; set; }
+        public decimal Accuracy { get; set; }
+        public string Damage { get; set; }
+        public int Range { get; set; }
+        public int FireRate { get; set; }
+        public int Mobility { get; set; }
+       // public Skin Skin { get; set; }
+
+        public Rifle(int id, string name)
+        {
+            WeaponId = id;
+            Name = name;
+        }
+    }
+}
