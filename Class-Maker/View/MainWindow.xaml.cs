07462183c8b45729dff772f11a163761be93251d
Aquerr <nerdianin@gmail.com>
2016-09-23 10:36:02 +0200

    Added background music.

--- Class-Maker/View/MainWindow.xaml.cs
+++ Class-Maker/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
 
 namespace Class_Maker.View
 {
@@ -7,9 +9,20 @@ namespace Class_Maker.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MediaPlayer backgroundMusicPlayer = new MediaPlayer();
+
         public MainWindow()
         {
             InitializeComponent();
+            backgroundMusicPlayer.Open(new Uri(@"C:\Users\nerdi\Desktop\Class-Maker\Class-Maker\Sounds\mw2Theme.wav"));
+            backgroundMusicPlayer.MediaEnded += new EventHandler(Media_Ended);
+            backgroundMusicPlayer.Play();
+        }
+
+        private void Media_Ended(object sender, EventArgs e)
+        {
+            backgroundMusicPlayer.Position = TimeSpan.Zero;
+            backgroundMusicPlayer.Play();
         }
     }
 }
