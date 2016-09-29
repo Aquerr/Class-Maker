da98e888dc4db1eaf7166cfd08c6ac721b14cbcc
Aquerr <nerdianin@gmail.com>
2016-09-29 11:49:24 +0200

    Fixed background music.

--- Class-Maker/View/MainWindow.xaml.cs
+++ Class-Maker/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Media;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -10,25 +12,33 @@ namespace Class_Maker.View
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MediaPlayer backgroundMusicPlayer = new MediaPlayer();
+        private readonly MediaPlayer _backgroundMusicPlayer = new MediaPlayer();
 
         public MainWindow()
         {
             InitializeComponent();
-            backgroundMusicPlayer.Open(new Uri(@"C:\Users\nerdi\Desktop\Class-Maker\Class-Maker\Sounds\mw2Theme.wav"));
-            backgroundMusicPlayer.MediaEnded += new EventHandler(Media_Ended);
-            backgroundMusicPlayer.Play();
-
+            StartBackgroundMusic();
+            //SoundPlayer soundPlayer = new SoundPlayer(@"..\..\Sounds\mw2theme.wav");
+            //     soundPlayer.PlayLooping();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void StartBackgroundMusic()
+        {
+            Uri uri = new Uri(@"..\..\Sounds\mw2Theme.wav", UriKind.Relative);
+            _backgroundMusicPlayer.Open(uri);
+            _backgroundMusicPlayer.MediaEnded += new EventHandler(Media_Ended);
+            _backgroundMusicPlayer.Play();
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
+
 	private void Media_Ended(object sender, EventArgs e)
         {
-            backgroundMusicPlayer.Position = TimeSpan.Zero;
-            backgroundMusicPlayer.Play();
+            _backgroundMusicPlayer.Position = TimeSpan.Zero;
+            _backgroundMusicPlayer.Play();
         }
         
     }
